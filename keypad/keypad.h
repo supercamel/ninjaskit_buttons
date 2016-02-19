3ceb058ac2053aaedd6b222e51ef532ac2a5f0c9
Samuel Cowen <samuel.cowen@camelsoftware.com>
2016-02-19 12:35:15 +1100

    Run astyle over everything.

--- keypad/keypad.h
+++ keypad/keypad.h
@@ -8,93 +8,93 @@ const int KEYPAD_EVENT_BUFFER_SIZE = 4;
 
 enum EVENT_TYPE
 {
-	BUTTON_PRESS_EVENT,
-	BUTTON_RELEASE_EVENT
+    BUTTON_PRESS_EVENT,
+    BUTTON_RELEASE_EVENT
 };
 
 struct KeypadEvent
 {
-	EVENT_TYPE type;
-	uint32 button;
-	etk::Time time;
+    EVENT_TYPE type;
+    uint32 button;
+    etk::Time time;
 };
 
 
 template <uint8 N_ROWS, uint8 N_COLS> class Keypad
 {
 public:
-	Keypad(gpio_pin row_pins[N_ROWS], gpio_pin col_pins[N_COLS]) : ringbuf(event_buffer, KEYPAD_EVENT_BUFFER_SIZE)
-	{
-		for(uint32 i = 0; i < N_ROWS; i++)
-		{
-			configure_as_input(row_pins[i], INPUT_PULLDOWN);
-			row_pin[i] = row_pins[i];
-		}
-		
-		for(uint32 i = 0; i < N_COLS; i++)
-		{
-			configure_as_output(col_pins[i]);
-			col_pin[i] = col_pins[i];
-		}
-		
-		for(auto i : etk::range(N_COLS))
-		{
-			for(auto j : etk::range(N_ROWS))
-				button_states[j][i] = false;
-		}
-	}
-	
-	void read_keypad()
-	{
-		for(auto i : etk::range(N_COLS))
-		{
-			set_pin(col_pin[i], true);
-			
-			for(auto j : etk::range(N_ROWS))
-			{
-				bool state = read_pin(row_pin[j]);
-				if(state != button_states[j][i])
-				{
-					//append event
-					KeypadEvent event;
-					event.time = etk::now();
-					event.button = j*N_ROWS + i;
-					state ? event.type = BUTTON_PRESS_EVENT : event.type = BUTTON_RELEASE_EVENT;
-					
-					ringbuf.put(event);
-					
-					button_states[j][i] = state;
-				}
-			}
-			
-			set_pin(col_pin[i], false);
-			
-			//pause for a *very* short moment just incase the lines are very long / have some stray capacitance and need a moment to switch low
-			//i really don't know this is necessary, but whatever.
-			while(etk::static_range(3))
-				asm("nop");
-		}
-	}
-	
-	uint32 get_n_events()
-	{
-		return ringbuf.available();
-	}
-	
-	KeypadEvent get_event()
-	{
-		if(get_n_events())
-			return ringbuf.get();
-		return KeypadEvent();
-	}
-	
+    Keypad(gpio_pin row_pins[N_ROWS], gpio_pin col_pins[N_COLS]) : ringbuf(event_buffer, KEYPAD_EVENT_BUFFER_SIZE)
+    {
+        for(uint32 i = 0; i < N_ROWS; i++)
+        {
+            configure_as_input(row_pins[i], INPUT_PULLDOWN);
+            row_pin[i] = row_pins[i];
+        }
+
+        for(uint32 i = 0; i < N_COLS; i++)
+        {
+            configure_as_output(col_pins[i]);
+            col_pin[i] = col_pins[i];
+        }
+
+        for(auto i : etk::range(N_COLS))
+        {
+            for(auto j : etk::range(N_ROWS))
+                button_states[j][i] = false;
+        }
+    }
+
+    void read_keypad()
+    {
+        for(auto i : etk::range(N_COLS))
+        {
+            set_pin(col_pin[i], true);
+
+            for(auto j : etk::range(N_ROWS))
+            {
+                bool state = read_pin(row_pin[j]);
+                if(state != button_states[j][i])
+                {
+                    //append event
+                    KeypadEvent event;
+                    event.time = etk::now();
+                    event.button = j*N_ROWS + i;
+                    state ? event.type = BUTTON_PRESS_EVENT : event.type = BUTTON_RELEASE_EVENT;
+
+                    ringbuf.put(event);
+
+                    button_states[j][i] = state;
+                }
+            }
+
+            set_pin(col_pin[i], false);
+
+            //pause for a *very* short moment just incase the lines are very long / have some stray capacitance and need a moment to switch low
+            //i really don't know this is necessary, but whatever.
+            while(etk::static_range(3))
+                asm("nop");
+        }
+    }
+
+    uint32 get_n_events()
+    {
+        return ringbuf.available();
+    }
+
+    KeypadEvent get_event()
+    {
+        if(get_n_events())
+            return ringbuf.get();
+        return KeypadEvent();
+    }
+
 private:
-	bool button_states[N_ROWS][N_COLS];
-	gpio_pin row_pin[N_ROWS];
-	gpio_pin col_pin[N_COLS];
-	
-	KeypadEvent event_buffer[KEYPAD_EVENT_BUFFER_SIZE];
-	etk::RingBuffer<KeypadEvent> ringbuf;
+    bool button_states[N_ROWS][N_COLS];
+    gpio_pin row_pin[N_ROWS];
+    gpio_pin col_pin[N_COLS];
+
+    KeypadEvent event_buffer[KEYPAD_EVENT_BUFFER_SIZE];
+    etk::RingBuffer<KeypadEvent> ringbuf;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -3,40 +3,40 @@
 
 int main()
 {
-	clock_setup();
-	
-	Serial1.begin(57600);
-	
-	//create a list of row pins
-	gpio_pin rows[] = { {PA, 0}, {PA, 1}, {PA, 2}, {PA, 3} };
-	
-	//create a list of column pins
-	gpio_pin cols[] = { {PA, 4}, {PA, 5}, {PA, 6}, {PA, 7 }};
-	
-	//create a keypad with four rows and four columns - 16 buttons total
-	Keypad<4, 4> keypad(rows, cols);
-							
-	while(true)
-	{
-		etk::sleep_ms(10);
-		
-		//read the state of the keypad and generate events if necessary
-		keypad.read_keypad();
-		
-		//while there are events queued up
-		while(keypad.get_n_events())
-		{
-			//get the next event
-			auto event = keypad.get_event();
-			
-			//if it's a button press
-			if(event.type == BUTTON_PRESS_EVENT)
-				Serial1.print("button ", event.button, " has been pressed\r\n");
-				
-			//if it's a button release
-			if(event.type == BUTTON_RELEASE_EVENT)
-				Serial1.print("button ", event.button, " has been released\r\n");
-		}
-	}
+    clock_setup();
+
+    Serial1.begin(57600);
+
+    //create a list of row pins
+    gpio_pin rows[] = { {PA, 0}, {PA, 1}, {PA, 2}, {PA, 3} };
+
+    //create a list of column pins
+    gpio_pin cols[] = { {PA, 4}, {PA, 5}, {PA, 6}, {PA, 7 }};
+
+    //create a keypad with four rows and four columns - 16 buttons total
+    Keypad<4, 4> keypad(rows, cols);
+
+    while(true)
+    {
+        etk::sleep_ms(10);
+
+        //read the state of the keypad and generate events if necessary
+        keypad.read_keypad();
+
+        //while there are events queued up
+        while(keypad.get_n_events())
+        {
+            //get the next event
+            auto event = keypad.get_event();
+
+            //if it's a button press
+            if(event.type == BUTTON_PRESS_EVENT)
+                Serial1.print("button ", event.button, " has been pressed\r\n");
+
+            //if it's a button release
+            if(event.type == BUTTON_RELEASE_EVENT)
+                Serial1.print("button ", event.button, " has been released\r\n");
+        }
+    }
 }
 
